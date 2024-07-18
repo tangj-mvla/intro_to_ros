a913da0c75309d6bb5f69e3ae89d030ef4e85933
tangj-mvla <joshuatang2007@gmail.com>
2024-07-18 11:30:15 -0400

    started movement

--- intro_to_ros/movement.py
+++ intro_to_ros/movement.py
@@ -3,10 +3,20 @@
 import rclpy
 import time
 from rclpy.node import Node
-
+from geometry_msgs.msg import Vector3Stamped
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
 class Movement(Node):
     def __init__(self):
         super.__init__("movement")
-        
+        qosProfile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.VOLATILE
+        )
+        self.create_publisher(
+            Vector3Stamped,
+            "/mavros/manual_control/send",
+            qosProfile
+        )
