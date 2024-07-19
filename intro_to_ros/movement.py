f03a454f05b602e18961ddb2035059bc2fa45281
tangj-mvla <joshuatang2007@gmail.com>
2024-07-19 09:30:33 -0400

    b

--- intro_to_ros/movement.py
+++ intro_to_ros/movement.py
@@ -3,14 +3,15 @@
 import rclpy
 import time
 from rclpy.node import Node
-from mavros_msgs.msg import OverrideRCIn, RCIn, RCOut
+from mavros_msgs.msg import OverrideRCIn, RCIn, RCOut, ManualControl
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
+from pymavlink import mavutil
 
 class Movement(Node):
     def __init__(self):
         super().__init__("movement")
         qos_profile = QoSProfile(
-            history=QoSHistoryPolicy.KEEP_LAST,
+            history=QoSHistoryPolicy.SYSTEM_DEFAULT,
             depth=10,
             reliability=QoSReliabilityPolicy.RELIABLE,
             durability=QoSDurabilityPolicy.VOLATILE
@@ -25,20 +26,23 @@ class Movement(Node):
         self.publisher_timer = self.create_timer(
             5.0, self.override_node
         )
+        self.get_logger().info("SUBSCRIBERS: " + str(self.publisher.get_subscription_count()))
 
     
     def override_node(self):
         msg = OverrideRCIn()
         
         msg.channels = [OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,
-                        OverrideRCIn.CHAN_NOCHANGE,1900,OverrideRCIn.CHAN_NOCHANGE,
+                        OverrideRCIn.CHAN_NOCHANGE,1700,OverrideRCIn.CHAN_NOCHANGE,
                         OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,
                         OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,
                         OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,
                         OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE,OverrideRCIn.CHAN_NOCHANGE]
         # msg.channels = [1000,1100,1200,1300,1400,1500,1600,1700]
-        # msg.rssi = 0
+        # msg.rssi = 0 
         self.publisher.publish(msg)
+        mav1 = mavutil.mavlink_connection()
+        mavutil.mavlink_connection(m)
         self.get_logger().info(f"Inputs: {msg.channels[0]}, {msg.channels[1]}")
 
         # forward: channel 5
