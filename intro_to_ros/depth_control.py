cd966d8e75c31e794665d5c0b4890623dc24dc92
tangj-mvla <joshuatang2007@gmail.com>
2024-07-25 08:54:37 -0400

    Fixed depth control

--- intro_to_ros/depth_control.py
+++ intro_to_ros/depth_control.py
@@ -44,8 +44,12 @@ class depthControl(Node):
     def measuredDepthCallback(self, msg):
         self.measured_depth = msg
         self.depth_control()
-        self.t1 = self.t2
-        self.t2 = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+        if (self.t1 == 0):
+            self.t1 = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+            self.t2 = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
+        else:
+            self.t1 = self.t2
+            self.t2 = msg.header.stamp.sec + msg.header.stamp.nanosec*1e-9
         self.get_logger().info(f"\nMeasured Depth: {msg.local}")
         
     def desiredDepthCallback(self, msg):
@@ -54,50 +58,49 @@ class depthControl(Node):
         self.get_logger().info(f"\nDesired Depth: {msg.local}")
     
     def depth_control(self):
-
         msg = ManualControl()
         # initial part, getting value, ect
-        measured_position = self.measured_depth.local
-        #desired_position = self.desired_depth.local
+        measured_position = self.measured_depth.local #og local
+        desired_position = self.desired_depth.local
         desired_position = 0.5
         dt = self.t2 - self.t1
-        self.get_logger().info(f"\nt1: {self.t1}")
+        self.get_logger().info(f"\n\nt1: {self.t1}")
         self.get_logger().info(f"\nt2: {self.t2}")
         self.get_logger().info(f"\ndt: {dt}")
-        if (dt == 0 or self.t1 == 0): # fix div by 0 problem
-            return
+        # if (dt == 0 or self.t1 == 0): # fix div by 0 problem
+        #     return
         # proportional control
-        Kp = 1.0
+        Kp = 175.0
         error = desired_position - measured_position
         self.get_logger().info(f"\nError: {error}")
         self.proportional = Kp * error
         self.get_logger().info(f"\nProportional: {self.proportional}")
 
         # integral control
-        Ki = 1.0
+        Ki = 175.0
         # self.integral = self.desired_depth - measured_position
         self.error_accumulator += error * dt # DT = TIME SINCE LAST UPDATE
         self.get_logger().info(f"\nError Accumulator: {self.error_accumulator}")
-        self.integral = min(max(Ki * self.error_accumulator, -1.0), 1.0) # PREVENTS INTEGRAL WINDUP PAST 1.0
+        self.integral = min(Ki * self.error_accumulator, 1.0) # PREVENTS INTEGRAL WINDUP PAST 1.0 & -1.0
         self.get_logger().info(f"\nIntegral: {self.integral}")
 
         # derivative control
-        Kd = 1.0
-        self.derivative = Kd * (error - self.previous_error) / dt 
+        Kd = 175.0
+        if (dt == 0): 
+            self.derivative = 0
+        else:
+            self.derivative = Kd * (error - self.previous_error) / dt 
         self.previous_error = error
+        self.get_logger().info(f"\nPrevious Error: {self.previous_error}")
         self.get_logger().info(f"\nDerivative: {self.derivative}")
 
         # add all & publish 
-        
-        # self.pid = (self.proportional + self.integral + self.derivative) * -1
         msg.x = 0.0
         msg.y = 0.0
         msg.z = 0.0
         msg.r = 0.0
-        msg.z = float((self.proportional + self.integral + self.derivative) * -1)
-        # msg.x = 0.5
+        msg.z = float((self.proportional + self.integral + self.derivative) * -1) + 50
         self.get_logger().info(f"\nPID: {msg.z}")
-        # msg.z = self.pid
         self.publisher.publish(msg)
         
 def main(args = None):
